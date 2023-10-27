20ed5a5aca689d3028a326f57610790b8f68d67e
Phuong Ha <pha1@ithaca.edu>
2023-10-27 13:40:52 -0400

    test message

--- app/__init__.py
+++ app/__init__.py
@@ -35,7 +35,7 @@ if not app.debug:
 
     if not os.path.exists('logs'):
         os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/bookstore.log', maxBytes=10240,
+    file_handler = RotatingFileHandler('logs/peerAdvisor.log', maxBytes=10240,
                                        backupCount=10)
     file_handler.setFormatter(logging.Formatter(
         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
