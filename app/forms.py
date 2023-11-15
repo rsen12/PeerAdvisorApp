ca78e08c68859aabfaa64b6a30b110687da33730
Phuong Ha <pha1@ithaca.edu>
2023-11-15 13:45:22 -0500

    not done advisor form

--- app/forms.py
+++ app/forms.py
@@ -20,6 +20,22 @@ class AdviseeForm(FlaskForm):
     submit = SubmitField('Submit')
 
 
+class AdvisorForm(FlaskForm):
+    name = StringField('Full Name', validators=[DataRequired()])
+    pronouns = StringField('Pronouns')
+    primary_advisor = SelectMultipleField(u'Primary Advisor(s)', coerce=int)
+    student_orgs = SelectMultipleField(u'Student Organization(s)', coerce=int)
+    interests = SelectMultipleField(u'Interest(s)', coerce=int)
+    preferred_contact_method = SelectField(u'Preferred Contact Method', choices=[('email', 'Email'), ('text', 'Text')])
+    class_year = SelectField(u'Class Year', validators=[DataRequired()], choices=[(2028, '2028'), (2027, '2027'), (2026, '2026'), (2025, '2025'), (2024, '2024'), (2023, '2023')], coerce=int)
+    major = SelectMultipleField(u'Major', validators=[DataRequired()], coerce=int)
+    minor = SelectMultipleField(u'Minor', validators=[DataRequired()], coerce=int)
+    internship = BooleanField('Internship?', validators=[DataRequired()])
+    study_abroad = BooleanField('Study Abroad?', validators=[DataRequired()])
+    research_exp = BooleanField('Research Experience?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+import csv
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
